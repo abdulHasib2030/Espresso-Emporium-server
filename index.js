44f50a74cfa5c484845ba5a4a16ca3c6983236f1
Abdul Hasib <abdulhasib2030@gmail.com>
2024-12-03 22:28:53 +0600

    password hide

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
-// NSpXaKHZisAcEjx6
-// coffeeMaster
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kpzks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -28,6 +27,7 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    const userCollection = client.db('coffeeDB').collection('users');
 
     app.get('/coffee', async(req, res)=>{
       const cursor = coffeeCollection.find();
@@ -78,6 +78,39 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/users', async(req, res)=>{
+      const cursor = userCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+      console.log(result);
+    })
+
+    app.post('/users', async(req, res)=>{
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await userCollection.insertOne(newUser)
+      res.send(result)
+    })
+
+    app.patch('/users', async(req, res)=>{
+      const email  = req.body.email
+      console.log(email);
+      const filter = {email}
+      const updatedDoc = {
+        $set:{
+          lastSignInTime: req.body?.lastSignTime
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+   
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
